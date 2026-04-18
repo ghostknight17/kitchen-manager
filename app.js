c5dd7fdd50c4791d1054544e5499ae6760eeef6f
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2026-04-18 00:51:38 -0300

    feat: estructura base de recetario y objeto ejemplo

--- app.js
+++ app.js
@@ -2,9 +2,82 @@ const contenido = document.getElementById("contenido");
 const btnRecetas = document.getElementById("btn-recetas");
 const btnInventario = document.getElementById("btn-inventario");
 const btnCalendario = document.getElementById("btn-calendario");
+const tartaDeChoclo = {
+nombre : "Tarta de choclo y garbanzos",
+ingredientes : [
+{
+  nombre: "pascualina",
+  cantidad: 1,
+  unidad: "u"
+},
+{
+nombre: "choclo en granos enlatado",
+cantidad: 1,
+unidad: "u"
+},
+{
+nombre: "choclo cremoso enlatado",
+cantidad: 1,
+unidad: "u",
+},
+{
+nombre: "garbanzos enlatados",
+cantidad: 2,
+unidad: "u"
+},
+{
+nombre: "cebolla",
+cantidad: 2,
+unidad: "u"
+},
+{
+nombre: "morron",
+cantidad: 1/2,
+unidad: "u"
+},
+{
+nombre: "huevo",
+cantidad: 3,
+unidad: "u"
+},
+{
+nombre: "leche",
+cantidad: 150,
+unidad: "g"
+},
+{
+nombre: "maicena",
+cantidad: 1,
+unidad: "cda"
+},
+{
+nombre: "sal",
+cantidad: null,
+unidad: null
+},
+{
+nombre: "pimienta",
+cantidad: null,
+unidad: null
+},
+{
+nombre: "nuez moscada",
+cantidad: null,
+unidad: null
+}
+],
+instrucciones : `Sofreir la cebolla y el morrón con una pizca de sal hasta que estén blandos. Agregar ambas latas de choclo, sumar los garbanzos (una lata procesarla con la cantidad necesaria de leche para que quede una pasta). Disolver la maicena en el resto de la leche fría y agregar, revolver hasta que espese, luego retirar del fuego. Cuando la mezcla esté tibia, agregar los huevos batidos con sal, pimienta y una pizca de nuez moscada. Armar la tarta y hornear a 180 ó 200 grados por 25/30 min.`
+};
+let recetario = [tartaDeChoclo];
 btnRecetas.addEventListener("click", function() {
     // Lógica para mostrar la sección de recetas
     contenido.innerHTML = "<h2>Recetas</h2><p>Aquí puedes encontrar deliciosas recetas para preparar en casa.</p>";
+    for (let i = 0; i < recetario.length; i++) {
+        const receta = recetario[i];
+        const recetasDiv = document.createElement("div");
+        recetasDiv.innerHTML = `<ul><li>${receta.nombre}</li></ul>`;
+        contenido.appendChild(recetasDiv);
+    }
 });
 btnInventario.addEventListener("click", function() {
     // Lógica para mostrar la sección de inventario
